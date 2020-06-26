a78f6e3cf5392648a9bd9325e98a21a8438daa2e
ts4blader <ts4blader@gmail.com>
2020-06-26 22:30:01 +0700

    modified:   src/main/java/GUI/MainMenu.java 	modified:   src/main/java/GUI/style.css

--- src/main/java/GUI/MainMenu.java
+++ src/main/java/GUI/MainMenu.java
@@ -3,25 +3,27 @@ package GUI;
 import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.File;
 
+
 public class MainMenu extends Application {
 
-    private final Font mainFont = Font.loadFont("src/main/resources/fonts/FiraSans-SemiBold.ttf",20);
 
     private VBox mainLayout;
 //    top Pane
-    private StackPane topPane;
+    private VBox topPane;
 //    mid Pane
     private HBox midPane;
     private JFXButton inputBtn;
@@ -32,10 +34,10 @@ public class MainMenu extends Application {
     private HBox botPane;
     
     private VBox devSection;
-    private Text devTitle;
-    private Text herobmdk;
-    private Text trisTran;
-    private Text nameLess;
+    private Label devTitle;
+    private Label herobmdk;
+    private Label trisTran;
+    private Label nameLess;
     
     private HBox contactPane;
     private File facebook = new File("src/main/resources/icons/facebook.png");
@@ -45,104 +47,114 @@ public class MainMenu extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         //====================== top Pane ======================
-        
             Text title = new Text("BroadCast Management");
-            title.setFont(mainFont);
-            title.setStyle("-fx-font-size: 30");
-            topPane = new StackPane(title);
+            title.setStyle("-fx-font-size: 40");
+            topPane = new VBox();
+            topPane.setSpacing(30);
             topPane.getStyleClass().addAll("topPane");
-            
+
                     
         //====================== End ======================
         
         //====================== mid pane ======================
 
+            int iconSize = 80;
+
             File inputImage = new File("src/main/resources/icons/electric-keyboard.png");
             ImageView inputImageView = new ImageView(new Image(inputImage.toURI().toURL().toString()));
-            inputImageView.setFitWidth(40);
-            inputImageView.setFitHeight(40);
+            inputImageView.setFitWidth(iconSize);
+            inputImageView.setFitHeight(iconSize);
 
             inputBtn = new JFXButton("Input");
             inputBtn.setGraphic(inputImageView);
-            inputBtn.setPadding(new Insets(10, 15, 10, 15));
+            inputBtn.setContentDisplay(ContentDisplay.TOP);
 
             File advancedImage = new File("src/main/resources/icons/database.png");
             ImageView advancedImageView = new ImageView(new Image(advancedImage.toURI().toURL().toString()));
-            advancedImageView.setFitHeight(40);
-            advancedImageView.setFitWidth(40);
+            advancedImageView.setFitHeight(iconSize);
+            advancedImageView.setFitWidth(iconSize);
 
             advancedBtn = new JFXButton("Advanced");
             advancedBtn.setGraphic(advancedImageView);
-            advancedBtn.setPadding(new Insets(10, 15, 10, 15));
+            advancedBtn.setContentDisplay(ContentDisplay.TOP);
 
             File updateImage = new File("src/main/resources/icons/exchange.png");
             ImageView updateImageView = new ImageView(new Image(updateImage.toURI().toURL().toString()));
-            updateImageView.setFitWidth(40);
-            updateImageView.setFitHeight(40);
+            updateImageView.setFitWidth(iconSize);
+            updateImageView.setFitHeight(iconSize);
 
             updateBtn = new JFXButton("Update");
             updateBtn.setGraphic(updateImageView);
-            updateBtn.setPadding(new Insets(10, 15, 10, 15));
+            updateBtn.setContentDisplay(ContentDisplay.TOP);
 
             File exitImage = new File("src/main/resources/icons/exit.png");
             ImageView exitImageView = new ImageView(new Image(exitImage.toURI().toURL().toString()));
-            exitImageView.setFitHeight(40);
-            exitImageView.setFitWidth(40);
+            exitImageView.setFitHeight(iconSize);
+            exitImageView.setFitWidth(iconSize);
 
             exitBtn = new JFXButton("Exit");
             exitBtn.setGraphic(exitImageView);
-            exitBtn.setPadding(new Insets(10, 15, 10, 15));
+            exitBtn.setContentDisplay(ContentDisplay.TOP);
 
-            midPane = new HBox(20);
-            midPane.setPadding(new Insets(20, 30, 20, 30));
+            midPane = new HBox(40);
+            midPane.setAlignment(Pos.CENTER);
+            midPane.getStyleClass().addAll("midPane");
             midPane.getChildren().addAll(inputBtn, advancedBtn, updateBtn, exitBtn);
+
+
+            topPane.getChildren().addAll(title, midPane);
+            topPane.setAlignment(Pos.CENTER);
                     
         //====================== End ======================
-        
         //====================== bot pane ======================
         
-            devTitle = new Text("Developer Team:");
-            herobmdk = new Text("herobmdk");
-            trisTran = new Text("Tris Tran");
-            nameLess = new Text("NamelessKing");
+            devTitle = new Label("Developer Team:");
+            devTitle.getStyleClass().addAll("title-dev");
+            herobmdk = new Label("herobmdk");
+            trisTran = new Label("Tris Tran");
+            nameLess = new Label("NamelessKing");
 
-            devSection = new VBox(2);
+            devSection = new VBox(3);
             devSection.getChildren().addAll(devTitle, herobmdk, trisTran, nameLess);
 
             ImageView facebookView = new ImageView(new Image(facebook.toURI().toURL().toString()));
             ImageView mailView = new ImageView(new Image(mail.toURI().toURL().toString()));
             ImageView gitView = new ImageView(new Image(github.toURI().toURL().toString()));
 
-            facebookView.setFitWidth(40);
-            facebookView.setFitHeight(40);
+            int contactIconSize = 40;
 
-            mailView.setFitWidth(40);
-            mailView.setFitHeight(40);
+            facebookView.setFitWidth(contactIconSize);
+            facebookView.setFitHeight(contactIconSize);
 
-            gitView.setFitWidth(40);
-            gitView.setFitHeight(40);
+            mailView.setFitWidth(contactIconSize);
+            mailView.setFitHeight(contactIconSize);
+
+            gitView.setFitWidth(contactIconSize);
+            gitView.setFitHeight(contactIconSize);
 
             contactPane = new HBox(facebookView, mailView, gitView);
-            contactPane.setSpacing(5);
+            contactPane.setSpacing(15);
+            contactPane.setAlignment(Pos.CENTER);
 
-            botPane = new HBox(20);
-            botPane.setPadding(new Insets(30));
+            botPane = new HBox(350);
+            botPane.getStyleClass().addAll("botPane");
+            botPane.setAlignment(Pos.CENTER);
+            botPane.setPadding(new Insets(25));
             botPane.getChildren().addAll(devSection, contactPane);
 
         //====================== End ======================
         
 
-        mainLayout = new VBox(topPane, midPane, botPane);
+        mainLayout = new VBox(topPane, botPane);
 
         primaryStage.setTitle("BroadCast Management");
-        primaryStage.setMinHeight(600);
         primaryStage.setMinWidth(800);
-        Scene scene = new Scene(mainLayout, 800, 600);
+        Scene scene = new Scene(mainLayout);
         File css = new File("src/main/java/GUI/style.css");
         scene.getStylesheets().add("file:///" + css.getAbsolutePath().replace("\\", "/"));
         primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 }
